3bafe9e31454c4c62a0945a89f830f2c3016d63d
jessicagrayson <grayjessica12@hotmail.com>
2023-08-17 16:21:52 +0000

    updates made to getBookData function preventing automatic refresh

--- js/main.js
+++ js/main.js
@@ -1,36 +1,27 @@
 // dom queries
 const $searchBar = document.querySelector('#query');
+const $apiEndpoint = 'https://openlibrary.org/search.json?q=';
 // const $icon = document.querySelector('.icon');
 // for testing only
 const $hersheyButton = document.querySelector('.hershey');
-const $api = 'https://openlibrary.org/search.json?q=';
 
 // handles search query from open library API
 function getBookData() {
+  const searchTerms = $searchBar.value;
+  const requestUrl = $apiEndpoint + searchTerms;
+  // DELETE THIS ALERT LATER, FOR TESTING
+  alert(searchTerms);
+  alert(requestUrl);
   // creates new xhr object
   const xhr = new XMLHttpRequest();
   // sets request method and URL
-  xhr.open('GET', 'https://openlibrary.org/search.json?q=');
+  xhr.open('GET', requestUrl);
   // gets body of HTTP response once it has been converted from JSON to JS objects
   xhr.responseType = 'json';
   // executes function when response is eventually loaded
-  xhr.addEventListener('load', function () {
-    // below for test purposes - DELETE BEFORE SUBMITTING PR
-    // console.log('status:', xhr.status);
-    // console.log('response:', xhr.response);
-  });
+  xhr.addEventListener('load', function () {});
   // sends request to the server at the URL specified in xhr.open()
   xhr.send();
 }
 
-getBookData();
-
-// function that gathers search terms from search bar
-function hersheyFunc(event) {
-  const searchTerms = $searchBar.value;
-  // DELETE THIS ALERT LATER, FOR TESTING
-  const hershey = $api + searchTerms;
-  alert(hershey);
-}
-
-$hersheyButton.addEventListener('click', hersheyFunc);
+$hersheyButton.addEventListener('click', getBookData);
