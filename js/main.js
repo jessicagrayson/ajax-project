563791be0588b161287c22258f1f1767d0e2306a
jessicagrayson <grayjessica12@hotmail.com>
2023-08-17 20:26:11 +0000

    added functionality preventing form from automatically resetting

--- js/main.js
+++ js/main.js
@@ -6,22 +6,28 @@ const $apiEndpoint = 'https://openlibrary.org/search.json?q=';
 const $hersheyButton = document.querySelector('.hershey');
 
 // handles search query from open library API
-function getBookData() {
+function getBookData(event) {
+  event.preventDefault();
   const searchTerms = $searchBar.value;
   const requestUrl = $apiEndpoint + searchTerms;
   // DELETE THIS ALERT LATER, FOR TESTING
-  alert(searchTerms);
-  alert(requestUrl);
+  // alert(searchTerms);
+  // alert(requestUrl);
   // creates new xhr object
   const xhr = new XMLHttpRequest();
   // sets request method and URL
   xhr.open('GET', requestUrl);
+  // console.log(requestUrl);
   // gets body of HTTP response once it has been converted from JSON to JS objects
   xhr.responseType = 'json';
   // executes function when response is eventually loaded
-  xhr.addEventListener('load', function () {});
+  xhr.addEventListener('load', function () {
+    // DELETE BELOW CONSOLE LOGS BEFORE PR
+    // console.log('status:', xhr.status);
+    // console.log('response:', xhr.response);
+  });
   // sends request to the server at the URL specified in xhr.open()
   xhr.send();
 }
-
+// click event for test button - submits form
 $hersheyButton.addEventListener('click', getBookData);
